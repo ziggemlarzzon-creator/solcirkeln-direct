8fbd82d5537ee68de62fc1020644d767e03e49a2
ziggemlarzzon-creator <zigge.m.larzzon@gmail.com>
2026-01-30 17:02:39 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,20 +3,11 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 
 export default defineConfig({
-  // GitHub Pages base path
   base: "/solcirkeln-direct/",
-
   plugins: [react()],
-
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "src"),
     },
-  },
-
-  build: {
-    // optional: improve build size & asset hashing
-    outDir: "dist",
-    sourcemap: false,
   },
 });
