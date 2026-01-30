51f4166cb49997020efb03ea09c9afae36fd337f
ziggemlarzzon-creator <zigge.m.larzzon@gmail.com>
2026-01-30 16:48:23 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,1 +1,7 @@
-base: "/solcirkeln-direct/",
+import { defineConfig } from "vite"
+import react from "@vitejs/plugin-react-swc"
+
+export default defineConfig({
+  base: "/solcirkeln-direct/",
+  plugins: [react()],
+})
